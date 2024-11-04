5da4e8bffa830f0fca5cb269673e58dd7a8722a7
luka.pintaric <luka_pintaric@toyota.ca>
2024-11-04 14:04:50 +0100

    Media

--- blocks/media/media.js
+++ blocks/media/media.js
@@ -1,7 +1,29 @@
 export default function decorate(block) {
-  let content = 
-  `<div>
-    <p>Some dummy content</p>
-  </div>`
+  const data = fetchData(block);
+
+  let content = `
+  <div>
+    <p>${data.title}</p>
+  </div>
+  `
+
   block.innerHTML = content;
 }
+
+const fetchData = (block) => {
+  const [titleContainer, referenceContainer] = block.children;
+  
+  const title = titleContainer?.querySelector('p')?.textContent.trim();
+  const reference = referenceContainer?.querySelector('a')?.getAttribute('href') ?? referenceContainer?.querySelector('p')?.textContent.trim();
+
+  const data = {
+    title: title,
+    reference: reference
+  };
+
+  return data;
+}
+
+<div>
+<p>Some dummy content</p>
+</div>
