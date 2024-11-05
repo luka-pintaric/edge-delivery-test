d57672d55043fab425312215085b9987e2d97175
luka.pintaric <luka_pintaric@toyota.ca>
2024-11-05 11:49:53 +0100

    Header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,11 +1,3 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
-
-/**
- * loads and decorates the header, mainly the nav
- * @param {Element} block The header block element
- */
 export default async function decorate(block) {
   const [wrapper] = block.children;
 
